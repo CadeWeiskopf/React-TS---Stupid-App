ecd89f99800c255c8608448f533a2afa55edd2b6
CadeWeiskopf <weiskopfcd@gmail.com>
2022-12-27 22:42:33 -0500

    dumb stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 
 function App() {
   const [count, setCount] = useState<number>(0);
   const handleSetCount = (newCount: number) => {
+    if (newCount < 0) return;
     setCount(newCount);
   };
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--count-background-color",
+      `rgba(${count * 2},${count * 2},${count * 2},1)`
+    );
+    document.documentElement.style.setProperty(
+      "--count-text-color",
+      `rgba(${255 - count * 2},${255 - count * 2},${255 - count * 2},1)`
+    );
+  }, [count]);
+
   return (
     <div className="App">
       {count}
